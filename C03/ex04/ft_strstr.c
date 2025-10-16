3f0a2e95ca764b260a5e7840bc98dd8f2072d18e
Boroianu-Andrei-Constantin-unilu <andrei.boroianu.001@student.uni.lu>
2025-10-16 21:32:10 +0000

    added C03 exercices

---
+++ C03/ex04/ft_strstr.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+char *ft_strstr(char *str, char *to_find) {
+	int	i = 0;
+	int	j = 0;
+
+	if (to_find[0] == '\0') {
+		return (str);
+    }
+	while (str[i]) {
+		while (str[i + j] == to_find[j] && str[i + j])
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+unsigned int ft_strlen(char *str) {
+	unsigned int len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size) {
+	unsigned int i;
+	unsigned int j = 0;
+	unsigned int dest_len;
+	unsigned int src_len;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size <= dest_len) {
+		return (size + src_len);
+    }
+	i = dest_len;
+	while (src[j] && (i + 1) < size) {
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
