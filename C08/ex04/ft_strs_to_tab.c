1e74663faf1a758f4efbe2d1069d4001e854f3eb
boroianu <andrei-constantin.boroianu@etu.unistra.fr>
2025-10-22 17:41:36 +0200

    corrected C08 exercices

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -1,72 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anboroia <anboroia@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 17:45:05 by anboroia          #+#    #+#             */
+/*   Updated: 2025/10/22 17:38:25 by anboroia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "ft_stock_str.h"
 
-static int  ft_strlen(char *s)
+static int	ft_strlen(char *s)
 {
-    while (s && s[i])
-        i++;
-    return (i);
+	int	i;
+
+	i = 0;
+	while (s && s[i])
+		i++;
+	return (i);
 }
 
-static char *ft_strdup(char *src)
+static char	*ft_strdup(char *src)
 {
-    int len;
-    char    *dup;
-    int i;
+	int		len;
+	char	*dup;
+	int		i;
 
-    dup = (char *)malloc(sizeof(char) * (len + 1));
-    if (!dup)
-        return (NULL);
-    i = 0;
-    len = ft_strlen(src);
-    while (i < len)
-    {
-        dup[i] = src[i];
-        i++;
-    }
-    dup[i] = '\0';
-    return (dup);
+	len = ft_strlen(src);
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
 }
 
-static void free_tab(t_stock_str *tab, int filled)
+static void	free_tab(t_stock_str *tab, int filled)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (i < filled)
-    {
-        if (tab[i].copy)
-        free(tab[i].copy);
-        i++;
-    }
-    free(tab);
+	i = 0;
+	while (i < filled)
+	{
+		if (tab[i].copy)
+			free(tab[i].copy);
+		i++;
+	}
+	free(tab);
 }
 
-struct  s_stock_str *ft_strs_to_tab(int ac, char **av)
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
-    t_stock_str *tab;
-    int i;
+	t_stock_str	*tab;
+	int			i;
 
-    tab = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
-    if (!tab)
-        return (NULL);
-
-    i = 0;
-    while (i < ac)
-    {
-        tab[i].size = ft_strlen(av[i]);
-        tab[i].str = av[i];
-        tab[i].copy = ft_strdup(av[i]);
-
-        if (tab[i].copy == NULL)
-        {
-            free_tab(tab, i);
-            return (NULL);
-        }
-        i++;
-    }
-    tab[i].str = 0;
-    tab[i].copy = 0;
-    tab[i].size = 0;
-    return (tab);
+	tab = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!tab)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		if (tab[i].copy == NULL)
+		{
+			free_tab(tab, i);
+			return (NULL);
+		}
+		i++;
+	}
+	tab[i].str = 0;
+	tab[i].copy = 0;
+	tab[i].size = 0;
+	return (tab);
 }
--- C08/ex05/ft_show_tab.c
+++ C08/ex05/ft_show_tab.c
@@ -1,49 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anboroia <anboroia@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 17:45:05 by anboroia          #+#    #+#             */
+/*   Updated: 2025/10/22 17:41:08 by anboroia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include "ft_stock_str.h"
 
-static void ft_putstr(char *s)
+static void	ft_putstr(char *s)
 {
-    if (!s)
-        return ;
-    while (*s)
-        write(1, s++, 1);
+	if (!s)
+		return ;
+	while (*s)
+		write(1, s++, 1);
 }
 
-static void ft_putnbr(int nb)
+static void	ft_putchar(char c)
 {
-    char    c;
-    
-    c = (nb % 10) + '0';
-    if (nb == -2147483648)
-    {
-        ft_putstr("-2147483648");
-        return ;
-    }
-    if (nb < 0)
-    {
-        write(1, '-', 1);
-        nb = -nb;
-    }
-    if (nb >= 10)
-        ft_putnbr(nb / 10);
-    ft_putchar(c);
+	write(1, &c, 1);
 }
 
-void    ft_show_tab(struct s_stock_str *par)
+static void	ft_putnbr(int nb)
 {
-    int i;
+	char	c;
 
-    if (!par)
-        return ;
-    i = 0;
-    while (par[i].str)
-    {
-        ft_putstr(par[i].str);
-        write(1, '\n', 1);
-        ft_putnbr(par[i].size);
-        write(1, '\n', 1);
-        ft_putstr(par[i].copy);
-        write(1, '\n', 1);
-        i++;
-    }
+	c = (nb % 10) + '0';
+	if (nb == -2147483648)
+	{
+		ft_putstr("-2147483648");
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar(c);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	if (!par)
+		return ;
+	i = 0;
+	while (par[i].str)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
+	}
 }
