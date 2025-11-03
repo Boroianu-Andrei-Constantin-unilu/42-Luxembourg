b7b44709b8870b21b55c66e41c3da67697fb89dd
Boroianu-Andrei-Constantin-unilu <andrei.boroianu.001@student.uni.lu>
2025-11-03 10:42:24 +0000

    added C11 exercices

---
+++ C11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anboroia <anboroia@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 17:45:05 by anboroia          #+#    #+#             */
+/*   Updated: 2025/10/22 18:44:13 by anboroia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int ft_strcmp(char *s1, char *s2)
+{
+    int i;
+
+    i = 0;
+    while (s1[i] && s2[i] && s1[i] == s2[i])
+        i++;
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void ft_sort_string_tab(char **tab)
+{
+    int     i;
+    int     j;
+    char    *temp;
+
+    i = 0;
+    while (tab[i])
+    {
+        j = i + 1;
+        while (tab[j])
+        {
+            if (ft_strcmp(tab[i], tab[j]) > 0)
+            {
+                temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+                sorted = 0;
+            }
+            j++;
+        }
+        i++;
+    }
+}
---
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anboroia <anboroia@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 17:45:05 by anboroia          #+#    #+#             */
+/*   Updated: 2025/10/22 18:44:13 by anboroia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+void    ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+    int     i;
+    int     j;
+    char    *temp;
+
+    i = 0;
+    while (tab[i])
+    {
+        j = i + 1;
+        while (tab[j])
+        {
+            if (cmp(tab[i], tab[j]) > 0)
+            {
+                temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
+}
