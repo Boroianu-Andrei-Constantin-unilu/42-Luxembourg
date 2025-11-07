dd896ed23c4c16946d1f65a6fd08b7071205a312
Andrei-Constantin Boroianu <anboroia@student.42luxembourg.lu>
2025-11-07 09:12:24 +0100

    gut

--- C11/ex06/ft_sort_string_tab.c
+++ C11/ex06/ft_sort_string_tab.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anboroia <anboroia@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 17:45:05 by anboroia          #+#    #+#             */
-/*   Updated: 2025/10/22 18:44:13 by anboroia         ###   ########.fr       */
+/*   Updated: 2025/11/07 09:11:55 by anboroia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,36 @@
 
 int ft_strcmp(char *s1, char *s2)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (s1[i] && s2[i] && s1[i] == s2[i])
-        i++;
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 void ft_sort_string_tab(char **tab)
 {
-    int     i;
-    int     j;
-    char    *temp;
+	int     i;
+	int     j;
+	char    *temp;
+	int     sorted;
 
-    i = 0;
-    while (tab[i])
-    {
-        j = i + 1;
-        while (tab[j])
-        {
-            if (ft_strcmp(tab[i], tab[j]) > 0)
-            {
-                temp = tab[i];
-                tab[i] = tab[j];
-                tab[j] = temp;
-                sorted = 0;
-            }
-            j++;
-        }
-        i++;
-    }
+	i = 0;
+	while (tab[i])
+	{
+		j = i + 1;
+		while (tab[j])
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+			{
+				temp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = temp;
+				sorted = 0;
+			}
+			j++;
+		}
+		i++;
+	}
 }
